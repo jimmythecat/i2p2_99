040b87ab743b96495445a6d9fe545e7fd9767227
Shan-Ze, Cheng <58551771+jimmythecat@users.noreply.github.com>
2024-12-11 03:55:28 +0800

    Update Game.h

--- SourceCode/Game.h
+++ SourceCode/Game.h
@@ -25,6 +25,10 @@ private:
 	 */
 	enum class STATE {
 		START, // -> LEVEL
+		//*-------*
+		// szc 12/11  新增Role Select
+		ROLE_SELECT,
+		//*-------*
 		LEVEL, // -> PAUSE, END
 		PAUSE, // -> LEVEL
 		END
@@ -33,6 +37,14 @@ private:
 	ALLEGRO_EVENT event;
 	ALLEGRO_BITMAP *game_icon;
 	ALLEGRO_BITMAP *background;
+	//*-------*
+	// szc 12/11  新增Role Select
+	ALLEGRO_BITMAP *role_select_background;  // 角色選擇畫面背景
+    ALLEGRO_BITMAP *role1_img;              // 角色1的按鈕圖片
+    ALLEGRO_BITMAP *role2_img;              // 角色2的按鈕圖片
+    ALLEGRO_BITMAP *role3_img;              // 角色3的按鈕圖片
+	int selected_role = -1;  //角色選擇
+	//*-------*
 private:
 	ALLEGRO_DISPLAY *display;
 	ALLEGRO_TIMER *timer;
